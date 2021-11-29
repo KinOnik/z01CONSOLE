590f88d7d6c4de4aaa2cea61840faabeebddcb7c
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-11-29 18:22:44 +0300

    Add files via upload

---
+++ 1consol/1_1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("\n\tВведите площадь полной поверхности куба: ");
+                double cubeArea = Convert.ToDouble(Console.ReadLine());
+                double edge = Math.Sqrt(cubeArea / 6);
+                Console.Write($"\n\tДлина ребра = {edge}\n\n");
+            }
+            catch (Exception)
+            {
+                Console.Write("\n\t\t****Введено не верное значение (НЕ ЧИСЛО)***\n\n");
+            }
+        }
+    }
+}
---
+++ 2consol/1_2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool errors = false;
+            try
+            {
+                int num;
+                Console.Write("\n\tВведите двухзначное число: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 10 || num > 99)
+                {
+                    errors = true;
+                    throw new Exception("\n\t\t****Число не двухнзначное!****\n\n");
+                }
+                if (((num % 10) + (num / 10)) % 2 == 0)
+                {
+                    Console.Write("\n\tСумма цифр числа четная\n\n");
+                }
+                else
+                {
+                    errors = true;
+                    throw new Exception("\n\t\t****Число нечетное!****\n\n");
+                }
+            }
+            catch (Exception ex) when (errors)
+            {
+                Console.Write(ex);
+            }
+            catch (Exception)
+            {
+                throw new Exception("\n\t\t****Введено не число****\n\n");
+            }
+        }
+    }
+}
